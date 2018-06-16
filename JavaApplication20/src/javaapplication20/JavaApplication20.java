b71cf5dc592f63f933c738bf1a0ddbf132909572
Trnewton <36055924+Trnewton@users.noreply.github.com>
2018-06-16 17:50:04 -0600

    Update JavaApplication20.java

--- JavaApplication20/src/javaapplication20/JavaApplication20.java
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -15,7 +15,7 @@ public class JavaApplication20 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Different test");
+        System.out.println("Different nah");
     }
     public static void newMethod(){
     }
