8cfdd3cbf3ab189fb7fa4e4d8e84f72e4d16c8ad
trnewton <thomas.newton@ucalgary.ca>
2018-06-24 14:25:58 -0600

    Completed dequeue method.

--- CPSC331/hw4/src/hw4/MazeSolver.java
+++ CPSC331/hw4/src/hw4/MazeSolver.java
@@ -10,92 +10,109 @@ package hw4;
  * @author thomasnewton
  */
 public class MazeSolver {
+
     private Vertex[] vertices;
     private double[] distArray;
     private Vertex[] queue;
     private int count;
-    
-    public MazeSolver(Vertex[] initVertices){
+
+    public MazeSolver(Vertex[] initVertices) {
         vertices = initVertices;
     }
-    
-    public void dijkstraSolve(int from, int to){
+
+    public void dijkstraSolve(int from, int to) {
         distArray = new double[vertices.length];
         queue = new Vertex[vertices.length];
         count = 0;
-        
-        for(Vertex v: vertices){
+
+        for (Vertex v : vertices) {
             Vertex copy = new Vertex(v);
             enqueue(copy, Double.POSITIVE_INFINITY);
         }
-        distArray[from-1] = 0;
-   }
-    
-    private double getDist(Vertex vertex){
-        return(distArray[vertex.id-1]);
+        distArray[from - 1] = 0;
     }
-    
-    private void enqueue(Vertex vertex, double dist){
+
+    private double getDist(Vertex vertex) {
+        return (distArray[vertex.id - 1]);
+    }
+
+    private void enqueue(Vertex vertex, double dist) {
         distArray[count] = dist;
         queue[count] = vertex;
-         
-        int parentIndex = (count-1)/2;
+
+        int parentIndex = (count - 1) / 2;
         Vertex parent = queue[parentIndex];
         double parentDist = getDist(parent);
-        
+
         int i = count;
-        while(i>=0 && dist<parentDist){
+        while (i >= 0 && dist < parentDist) {
             Vertex temp = parent;
             queue[parentIndex] = vertex;
             queue[i] = temp;
             i = parentIndex;
-            parentIndex = (int) Math.floor((i-1)/2);
+            parentIndex = (int) Math.floor((i - 1) / 2);
             parent = queue[parentIndex];
             parentDist = getDist(parent);
         }
         count++;
     }
-    
-    private Vertex dequeue(){
+
+    private Vertex dequeue() {
         Vertex root = queue[0];
-        
-        for(int i=1;i<queue.length;i++){
-            queue[i-1] = queue[i];
-        }
+        queue[0] = queue[count];
         queue[count] = root;
         count--;
-        //Restore heap
-        return(root);
+
+        int i = 0;
+        int firstLeaf = (count / 2) - 2;
+        while (i > firstLeaf) {
+            if (getDist(queue[i]) > getDist(queue[(2 * i) - 1])) {
+                int childI = (2 * i) - 1;
+                Vertex temp = queue[i];
+                queue[i] = queue[childI];
+                queue[childI] = temp;
+                i = childI;
+            } else if (getDist(queue[i]) > getDist(queue[(2 * i) - 2])) {
+                int childI = (2 * i) - 2;
+                Vertex temp = queue[i];
+                queue[i] = queue[childI];
+                queue[childI] = temp;
+                i = childI;
+            } else {
+                break;
+            }
+        }
+        return (root);
     }
-    
-    private void heapify(){
-        int lastNonLeaf = (count/2)-1;
-        if(count%2!=0){
+
+    private void heapify() {
+        int lastNonLeaf = (count / 2) - 1;
+        if (count % 2 != 0) {
             double nodeDist = getDist(queue[lastNonLeaf]);
-            double childDist = getDist(queue[(2*lastNonLeaf)+1]);
-            if(nodeDist > childDist){
+            double childDist = getDist(queue[(2 * lastNonLeaf) + 1]);
+            if (nodeDist > childDist) {
                 Vertex temp = queue[lastNonLeaf];
-                queue[lastNonLeaf] = queue[(2*lastNonLeaf)+1];
-                queue[(2*lastNonLeaf)+1] = temp;
+                queue[lastNonLeaf] = queue[(2 * lastNonLeaf) + 1];
+                queue[(2 * lastNonLeaf) + 1] = temp;
             }
             lastNonLeaf++;
         }
-        for(int i=lastNonLeaf; i>-1;i--){
-            double lChildDist = getDist(queue[(2*i)+1]);
-            double rChildDist = getDist(queue[(2*i)+2]);
+        for (int i = lastNonLeaf; i > -1; i--) {
+            double lChildDist = getDist(queue[(2 * i) + 1]);
+            double rChildDist = getDist(queue[(2 * i) + 2]);
             double nodeDist = getDist(queue[i]);
-                    
-            if(nodeDist > lChildDist){
+
+            if (nodeDist > lChildDist) {
                 Vertex temp = queue[i];
-                queue[i] = queue[(2*i)+1];
-                queue[(2*i)+1] = temp;
+                queue[i] = queue[(2 * i) + 1];
+                queue[(2 * i) + 1] = temp;
                 nodeDist = getDist(queue[i]);
             }
-            
-            if(nodeDist > rChildDist){
+
+            if (nodeDist > rChildDist) {
                 Vertex temp = queue[i];
-                queue[i] = queue[(2*i)+2];
-                queue[(2*i)+2] = temp;
+                queue[i] = queue[(2 * i) + 2];
+                queue[(2 * i) + 2] = temp;
             }
         }
     }
